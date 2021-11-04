0350266ded3bd1d15eabcc32563d4e09b133b31e
Huy Nguyen <danghuy1999@gmail.com>
2021-11-05 06:35:05 +0700

    Finish

--- applications/consumer/index.js
+++ applications/consumer/index.js
@@ -1,4 +1,7 @@
 const { Kafka } = require("kafkajs");
+const WebSocket = require("ws");
+
+const ws = new WebSocket("wss://ws.coinapi.io/v1/");
 
 const kafka = new Kafka({
   clientId: "my-app",
@@ -6,7 +9,7 @@ const kafka = new Kafka({
 });
 
 const producer = kafka.producer();
-const consumer = kafka.consumer({ groupId: "test-group" });
+const consumer = kafka.consumer({ groupId: "coin-topic" });
 const admin = kafka.admin();
 
 const run = async () => {
@@ -16,25 +19,44 @@ const run = async () => {
 
   await admin.createTopics({
     waitForLeaders: true,
-    topics: [{ topic: "test-topic" }],
+    topics: [{ topic: "coin-topic" }],
   });
+
   // Producing
 
-  for (let i = 0; i < 10; i++) {
+  ws.on("open", function open() {
+    const hello = {
+      type: "hello",
+      apikey: "BD0D2409-19C8-4235-9FA0-A45BDE164F5E",
+      heartbeat: false,
+      subscribe_data_type: ["trade"],
+      subscribe_filter_symbol_id: ["COINBASE_SPOT_ETH_USD$"],
+    };
+    ws.send(JSON.stringify(hello));
+  });
+
+  ws.on("message", function incoming(data) {
+    const coin = JSON.parse(data.toString());
+    const message = {
+      symbol_id: coin.symbol_id,
+      price: coin.price,
+      time_coinapi: coin.time_coinapi,
+    };
     producer.send({
-      topic: "test-topic",
-      messages: [{ value: `Hello KafkaJS user number ${i}` }],
+      topic: "coin-topic",
+      messages: [{ value: JSON.stringify(message) }],
     });
-  }
+  });
 
   // // Consuming
   await consumer.connect();
 
-  await consumer.subscribe({ topic: "test-topic", fromBeginning: true });
+  await consumer.subscribe({ topic: "coin-topic", fromBeginning: true });
 
   await consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
       console.log({
+        topic,
         partition,
         offset: message.offset,
         value: message.value.toString(),
